d6f6180ea29a15ac23aa1830d6d7e45019c9ff2d
Petar Popovic <petarpnp@gmail.com>
2025-02-21 14:02:13 +0100

    Add coorganizers

--- src/app/page.js
+++ src/app/page.js
@@ -12,15 +12,25 @@ const dela = Dela_Gothic_One({
 
 export default function Home() {
   return (
-    <div className="h-[100vh] realtive flex items-center justify-center">
-      <div className="absolute w-full h-full bg-[url(/pat.svg)] opacity-30 z-0" />
-      <div className="mx-5 my-50 z-10">
+    <div className="min-h-[100vh] realtive bg-[url(/pat-30-opacity.svg)] p-10">
+      <div className="max-w-3xl mx-auto my-50 z-10 py-[50px]">
         <p className={`uppercase font-bold ${dela.className} text-lg lg:text-2xl`}>The first conference for devrels</p>
         <div>
           packed with talks, panels, and networking
         </div>
         <img className="mt-2" src="/web3devrel.svg" />
         <p className="uppercase bg-black inline-block text-sm sm:text-base text-white px-7 py-2 rounded-full mt-2">summit, {EVENT_DATE} - Belgrade</p>
+        <div className="mt-10">
+          <p>Organizers:</p>
+          <div className="flex items-center gap-5 sm:gap-10 mt-3">
+            <a href="https://ethbelgrade.rs/" target="_blank" rel="noreferrer noopener">
+              <img className="w-full max-w-[200px] max-h-[40px]" src="/eth-belgrade-logo.svg" alt="ETH Belgrade Logo" />
+            </a>
+            <a href="https://devreluni.com/" target="_blank" rel="noreferrer noopener">
+              <img className="w-full max-w-[200px] max-h-[40px]" src="/devreluni-black.png" alt="DevRelUni Logo" />
+            </a>
+          </div>
+        </div>
         <div className="sm:flex items-center justify-between py-10 mt-2">
           <p className={`mb-3 sm:mb-0`}><b>Have something to say?</b> Speaker applications are open</p>
           <a href="https://forms.gle/DQWw8XviBzNDCWag9" target="_blank" rel="noreferrer noopener">
